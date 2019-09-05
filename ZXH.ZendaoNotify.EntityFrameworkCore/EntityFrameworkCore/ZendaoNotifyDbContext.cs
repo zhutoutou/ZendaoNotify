eaa9b8285570679dd9566e5c2cb8ef9e93ff2aa3
zhutoutou <30338453+zhutoutou@users.noreply.github.com>
2019-09-05 15:27:55 +0800

    fix bug no reference

--- ZXH.ZendaoNotify.EntityFrameworkCore/EntityFrameworkCore/ZendaoNotifyDbContext.cs
+++ ZXH.ZendaoNotify.EntityFrameworkCore/EntityFrameworkCore/ZendaoNotifyDbContext.cs
@@ -1,8 +1,21 @@
 using Abp.Zero.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ZXH.ZendaoNotify.Core.Authorization.Roles;
+using ZXH.ZendaoNotify.Core.Authorization.Users;
+using ZXH.ZendaoNotify.Core.MultiTenancy;
 
 namespace ZXH.ZendaoNotify.EntityFrameworkCore.EntityFrameworkCore
 {
-    public class ZendaoNotifyDbContext:AbpZeroDbContext<Tenant,Role>{
+    public class ZendaoNotifyDbContext : AbpZeroDbContext<Tenant, Role, User, ZendaoNotifyDbContext>
+    {
 
+        public ZendaoNotifyDbContext(DbContextOptions<ZendaoNotifyDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
